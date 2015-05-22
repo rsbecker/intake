d0946ad7084cdefcd1b77a6d05a024191bd4b14e
Randall S. Becker <rsbecker@nexbridge.com>
2015-05-22 00:17:21 -0500

    Added import from NSK based on githints

--- absorb.pl
+++ absorb.pl
@@ -12,13 +12,23 @@ while(<NskMirror>) {
 }
 close(NskMirror);
 
-print "Importing ", $subvol, " into ", getcwd, "\n";
+my $workingcache=getcwd;
+print "Importing ", $subvol, " into ", $workingcache, "\n";
 
 my $githints = $subvol . "/githints";
+my $cachehints = $workingcache . "/githints";
+
+print "Importing githints\n";
+system("./fromnsk --from=$githints --to=$cachehints");
 
 open(GitHintsFile, "<", $githints) or die "Missing GITHINTS: $!";
 while(<GitHintsFile>) {
 	chomp;
-	print "Got '", $_, "'\n";
+	my $str = $_;
+	my($nskname, $ossname, $filecode) = split / +/, $str;
+	my $fromname = $subvol . "/" . $nskname;
+	my $toname = $workingcache . "/" . $ossname;
+	print "Importing ", $fromname, " to ", $toname, "\n";
+	system("./fromnsk --from=$fromname --to=$toname");
 }
 close(GitHintsFile);
